725b55089fe3eddbe18690d23369071a84ce5f5a
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-05-05 18:30:24 +0700

    add requirements.txt

--- webbot/settings.py
+++ webbot/settings.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -16,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'herokuapp',
 ]
 
 MIDDLEWARE = [
