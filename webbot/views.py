9cdc9bc65fe2c0d709ffa99c7a750d152c554ef5
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-05-05 15:33:28 +0700

    pertama

---
+++ webbot/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import joblib
+
+def index(request):
+    return render(request, 'webbot/index.htm')
+
+def bot_search(request):
+    query = request.GET.get('query')
+    filename = '/home/vika/sentimen analisis/results/vektor.sav'
+    filename1 = '/home/vika/sentimen analisis/results/MNB.sav'
+    vektor = joblib.load(filename)
+    model = joblib.load(filename1)
+    result = model.predict(vektor.transform([query]))
+    if (result == [2]) :
+        ans = "negatif"
+    elif (result == [1]) :
+        ans = "positif"
+    else:
+        ans = 'Nothing'
+
+    return render(request, 'webbot/index.htm', {'ans': ans, 'query': query})
